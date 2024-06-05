df2171efd680e20b35183b6a365cd7c483d8ada0
ask250 <aslrahidov250@gmail.com>
2024-06-05 13:26:07 +0500

    fs module

--- modules/fs.js
+++ modules/fs.js
@@ -1,5 +1,30 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
+
+// fs.mkdir(path.join(__dirname, "templates"), err => {
+// 	if (err) throw new Error()
+// 	console.log("folder create successfully")
+// })
+
+// fs.mkdir(path.join(__dirname, "notes"), err => {
+// 	if (err) throw new Error()
+// 	console.log("folder create successfully")
+// })
 
 
-fs.mkdir()
+fs.writeFile(path.join(__dirname, "notes", "may.txt"), "hello world", err => {
+	if (err) throw new Error()
+	console.log("file create successfully")
+
+	fs.appendFile(path.join(__dirname, "notes", "may.txt"), "hello again", err => {
+		if (err) throw new Error()
+		console.log("file append successfully")
+		fs.readFile(path.join(__dirname, "notes", "may.txt"),"utf-8", (err, data) => {
+			if (err) throw new Error()
+			console.log(data)
+		})
+	})
+
+})
+
+
