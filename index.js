ca5502ff38b29627e6a41718e470a8df6028ed4a
ask250 <aslrahidov250@gmail.com>
2024-06-09 16:22:01 +0500

    HTTP module

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+	//req - serverga so'rov
+	//res - server javobi
+	console.log(req.url)
+	
+	res.write("<h2>hello</h2>")
+	res.end()
+})
+
+server.listen(8000, () => {
+	console.log("server's port 8000")
+})
