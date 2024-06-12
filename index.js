ed5bd17d5a482f190594ef4216ebc1faa806db2e
ask250 <aslrahidov250@gmail.com>
2024-06-12 13:07:35 +0500

    Web Server

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-	//req - serverga so'rov
-	//res - server javobi
-	console.log(req.url)
-	
-	res.write("<h2>hello 3</h2>")
-	res.end()
+	if (req.method === 'GET') {
+		res.writeHead(200, {"Content-Type": "text/html"})
+		res.end(`
+		<h1>Send Email</h1>
+		<form action="/" method="POST">
+			<label for="name">Name</label>
+			<input type="text" id="name" name="name">
+			<label for="email">Email</label>
+			<input type="email" id="email" name="email">
+			<button type="submit">Submit</button>
+		</form>
+		`)
+	} else if (req.method === 'POST') {
+		res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
+		const email = []
+		
+		req.on("data", (data) => {
+			email.push(Buffer.from(data))
+		})
+		req.on("end", () => {
+			const massage = email.toString().split('=')[1]
+			res.end(`Thank you for your email ${massage}`)
+			
+		})
+		
+		
+	}
 })
 
 server.listen(8000, () => {
